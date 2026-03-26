77c43dcb98b997d8eee09228fde019b3eee04526
Life is Hard <mostima0@outlook.com>
2026-03-26 11:00:09 +0000

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let app = MainWindow::new()?;
-
     app.run()
 }
